6844395d4d8a58b79a5cb077eead8223d7ed0d91
Marc vd Made <marcvdmade01@gmail.com>
2022-04-04 00:42:26 +0200

    fixed some stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let fetchAllUrl = 'https://cmgt.hr.nl/api/projects';
+let fetchUrl = fetchAllUrl
+
 window.onload = () => {
     console.log('starting app...');
     init()
@@ -9,7 +12,7 @@ async function init() {
 }
 
 async function getProjects() {
-    let url = 'https://cmgt.hr.nl/api/projects'
+    let url = fetchUrl
     let projects = [];
     
     try {
@@ -27,20 +30,23 @@ async function renderProjects() {
     let projects = await getProjects();
     console.log(projects)
     let projectsHolder = document.getElementById('projects-holder');
+    projectsHolder.innerHTML = '';
 
     projects.forEach(project => {
         // Create the card for a project
         let projectCard = document.createElement('div');
 
         // Used for appending tailwind css classes (thx js)
-        addTailwind(['border-solid', 'border-4', 'border-black', 'rounded-xl', 'w-96', 'dark:border-white'], projectCard);
+        addTailwind(['border-solid', 'border-4', 'border-black', 'bg-white', 'dark:bg-black', 'rounded-xl', 'w-96', 'dark:border-red-600', 'hover:drop-shadow-2xl', 'hover:-translate-y-5', 'transition', 'duration-200'], projectCard);
 
         // Get project details and append them to card
 
         // Image
-        let img = document.createElement('img');
-        img.setAttribute('src', project.project.header_image[0]);
-        addTailwind(['rounded-t-lg', 'w-full'], img);
+        let img = document.createElement('div');
+        img.style.backgroundImage = `url(${project.project.header_image[0]}`;
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundSize = '100% 80%'
+        addTailwind(['rounded-t-lg', 'w-full', 'h-64'], img);
 
         // Text holder
         let infoDiv = document.createElement('div');
@@ -65,7 +71,7 @@ async function renderProjects() {
         project.project.tags.forEach((tag) => {
             let button = document.createElement('button');
             button.innerHTML = tag.name;
-            addTailwind(['px-2', 'py-1', 'rounded-xl', 'bg-red-600', 'text-white'], button);
+            addTailwind(['px-2', 'py-1', 'rounded-xl', 'bg-red-600', 'text-white', 'hover:bg-red-500'], button);
             tagDiv.appendChild(button);
         });
         infoDiv.appendChild(tagDiv)
@@ -98,11 +104,12 @@ async function renderTags() {
     let tags = await getTags();
     console.log(tags)
     let tagsHolder = document.getElementById('tags-holder');
+    tagsHolder.innerHTML = '';
 
     tags.forEach((tag) => {
         let button = document.createElement('button');
         button.innerHTML = tag.name;
-        addTailwind(['px-2', 'py-1', 'rounded-xl', 'bg-red-600', 'text-white'], button);
+        addTailwind(['px-2', 'py-1', 'rounded-xl', 'bg-red-600', 'text-white', 'hover:bg-red-500'], button);
         tagsHolder.appendChild(button);
     })
 }
